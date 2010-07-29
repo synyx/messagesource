c0b00c9ef9275916a16b5aed3c28a606e41c3879
Marc Kannegiesser <kannegiesser@synyx.de>
2010-07-29 12:41:20 +0000

    * reintroduced importMessages() method

--- src/main/java/org/synyx/messagesource/importer/Importer.java
+++ src/main/java/org/synyx/messagesource/importer/Importer.java
@@ -4,9 +4,11 @@
 package org.synyx.messagesource.importer;
 
 import java.io.File;
+import java.io.PrintStream;
 
 import org.synyx.messagesource.MessageAcceptor;
 import org.synyx.messagesource.MessageProvider;
+import org.synyx.messagesource.Messages;
 import org.synyx.messagesource.filesystem.FileSystemMessageProvider;
 
 
@@ -41,4 +43,35 @@ public class Importer {
         target.setMessages(basename, source.getMessages(basename));
     }
 
+
+    public static void main(String[] args) {
+
+        PrintStream out = System.out;
+        out.println("Messageimporter");
+        if (args.length < 2) {
+            System.err.println("Usage: MessageImporter <basepath> <basename> [<basename2> [ <basename3> ... ] ]");
+        }
+        String dir = null;
+        for (String basename : args) {
+            if (dir == null) {
+                dir = basename;
+                continue;
+            }
+            FileSystemMessageProvider importer = new FileSystemMessageProvider(new File(dir).getAbsolutePath());
+            Messages messages = importer.getMessages(basename);
+
+            printMessages(messages, basename, out);
+        }
+    }
+
+
+    public void importMessages() {
+
+        for (String basename : source.getAvailableBaseNames()) {
+            importMessages(basename);
+        }
+
+        // TODO think about basenames that exist in target but not in source
+
+    }
 }

223d6e6215c62cc741cdca88bb223605d6f7a6a4
Marc Kannegiesser <kannegiesser@synyx.de>
2010-07-29 12:41:52 +0000

    * bugfix

--- src/main/java/org/synyx/messagesource/importer/Importer.java
+++ src/main/java/org/synyx/messagesource/importer/Importer.java
@@ -4,11 +4,9 @@
 package org.synyx.messagesource.importer;
 
 import java.io.File;
-import java.io.PrintStream;
 
 import org.synyx.messagesource.MessageAcceptor;
 import org.synyx.messagesource.MessageProvider;
-import org.synyx.messagesource.Messages;
 import org.synyx.messagesource.filesystem.FileSystemMessageProvider;
 
 
@@ -41,27 +39,6 @@ public class Importer {
     public void importMessages(String basename) {
 
         target.setMessages(basename, source.getMessages(basename));
-    }
-
-
-    public static void main(String[] args) {
-
-        PrintStream out = System.out;
-        out.println("Messageimporter");
-        if (args.length < 2) {
-            System.err.println("Usage: MessageImporter <basepath> <basename> [<basename2> [ <basename3> ... ] ]");
-        }
-        String dir = null;
-        for (String basename : args) {
-            if (dir == null) {
-                dir = basename;
-                continue;
-            }
-            FileSystemMessageProvider importer = new FileSystemMessageProvider(new File(dir).getAbsolutePath());
-            Messages messages = importer.getMessages(basename);
-
-            printMessages(messages, basename, out);
-        }
     }
 
 

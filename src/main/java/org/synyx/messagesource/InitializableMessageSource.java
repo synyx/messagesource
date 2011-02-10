c595130f78b1ae2dd223f912a8d9a66d728bfbbc
Marc Kannegiesser <kannegiesser@synyx.de>
2011-02-10 18:21:11 +0000

    [maven-release-plugin]  copy for tag messagesource-0.5

--- src/main/java/org/synyx/messagesource/InitializableMessageSource.java
+++ src/main/java/org/synyx/messagesource/InitializableMessageSource.java
@@ -8,8 +8,10 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.support.AbstractMessageSource;
+import org.springframework.util.Assert;
 import org.synyx.messagesource.util.LocaleUtils;
 
 
@@ -23,30 +25,55 @@ import org.synyx.messagesource.util.LocaleUtils;
  * <li>given {@link Locale}s language + country + variant</li>
  * <li>given {@link Locale}s language + country</li>
  * <li>given {@link Locale}s language</li>
- * <li>VM-wide default {@link Locale}s language + country + variant</li>
- * <li>VM-wide default {@link Locale}s language + country</li>
- * <li>VM-wide default {@link Locale}s language</li>
+ * <li>default {@link Locale}s language + country + variant (property defaultLocale, if not null)</li>
+ * <li>default {@link Locale}s language + country (property defaultLocale, if not null)</li>
+ * <li>default {@link Locale}s language (property defaultLocale, if not null)</li>
  * <li>Default (basename)</li>
  * </ul>
+ * <p>
+ * You may set no defaultLocale which leads to resolving without the lines above containing (property defaultLocale, if
+ * not null).
+ * </p>
+ * <p>
+ * You must set a {@link MessageProvider} which gets called to read all the messages at once.
+ * </p>
+ * <p>
+ * You may set a basename or a List of basenames explicitly. If you do so this only messages for this basename(s) are
+ * resolved (and read from the {@link MessageProvider}). If you do not provide a basename all the messages delivered
+ * from the {@link MessageProvider} are used.
+ * </p>
  * 
  * @author Marc Kannegiesser - kannegiesser@synyx.de
  */
-public class InitializableMessageSource extends AbstractMessageSource {
+public class InitializableMessageSource extends AbstractMessageSource implements InitializingBean {
 
     protected Map<Locale, List<String>> resolvingPath = new HashMap<Locale, List<String>>();
     protected Map<String, Map<String, MessageFormat>> messages;
-    protected Locale defaultLocale = Locale.getDefault();
+    protected Locale defaultLocale;
 
     protected MessageProvider messageProvider;
     protected List<String> basenames = new ArrayList<String>();
 
+    /**
+     * If this property is set to true this initializes post-construction (spring lifecycle interface)
+     */
+    protected boolean autoInitialize = true;
+
+    /**
+     * Property that indicates if all basenames returned from the {@link MessageProvider} should be used (=false) or the
+     * ones explicitely set using {@link #setBasename(String)} or {@link #setBasenames(List)}.
+     */
     protected boolean basenameRestriction = false;
 
 
     /**
-     * Initializes messages by retrieving them from the set {@link MessageProvider}
+     * Initializes messages by retrieving them from the set {@link MessageProvider}. This also leads to a reset of the
+     * resolving-paths used to cache lookup-paths for messages
      */
     public void initialize() {
+
+        // reset the path-cache (default-locale could have been changed)
+        resolvingPath = new HashMap<Locale, List<String>>();
 
         if (!basenameRestriction) {
             basenames = new ArrayList<String>();
@@ -61,6 +88,11 @@ public class InitializableMessageSource extends AbstractMessageSource {
     }
 
 
+    /**
+     * Reads all messages from the {@link MessageProvider} for the given Basename
+     * 
+     * @param basename the basename to initialize messages for
+     */
     protected void initialize(String basename) {
 
         Messages messagesForBasename = messageProvider.getMessages(basename);
@@ -143,24 +175,45 @@ public class InitializableMessageSource extends AbstractMessageSource {
     }
 
 
-    protected Locale getDefaultLocale() {
+    public Locale getDefaultLocale() {
 
         return defaultLocale;
     }
 
 
+    /**
+     * Sets the default {@link Locale} used during message-resolving. If for a given Locale the message is not found the
+     * message gets looked up for the default-locale. If the message is not found then the "base-message" is used. This
+     * is allowed to be null which then means "no default locale"
+     * 
+     * @param defaultLocale the Locale to use as default or null if no default-locale should be used
+     */
     public void setDefaultLocale(Locale defaultLocale) {
 
         this.defaultLocale = defaultLocale;
     }
 
 
+    /**
+     * Sets the {@link MessageProvider} for this which is asked for all its Messages during initialisation.
+     * 
+     * @param messageProvider the {@link MessageProvider} to use
+     */
     public void setMessageProvider(MessageProvider messageProvider) {
+
+        Assert.notNull(messageProvider);
 
         this.messageProvider = messageProvider;
     }
 
 
+    /**
+     * Sets a single basename for this. This cannot be used in combination with {@link #setBasenames(List)}. If neither
+     * {@link #setBasename(String)} nor {@link #setBasenames(List)} is called the basenames are looked up from the
+     * {@link MessageProvider}
+     * 
+     * @param basename the single basename to use for this instance
+     */
     public void setBasename(String basename) {
 
         basenameRestriction = true;
@@ -169,12 +222,44 @@ public class InitializableMessageSource extends AbstractMessageSource {
     }
 
 
+    /**
+     * Sets a {@link List} of basenames to use for this instance. This cannot be used in combination with
+     * {@link #setBasename(String)}. If neither {@link #setBasename(String)} nor {@link #setBasenames(List)} is called
+     * the basenames are looked up from the {@link MessageProvider}
+     * 
+     * @param basenames the {@link List} of basenames
+     */
     public void setBasenames(List<String> basenames) {
+
+        Assert.notNull(basenames);
 
         if (!basenames.isEmpty()) {
             basenameRestriction = true;
             this.basenames = basenames;
         }
+    }
+
+
+    /**
+     * Callback to call {@link #initialize()} after construction of this using a Spring-Callback
+     */
+    public void afterPropertiesSet() throws Exception {
+
+        if (autoInitialize) {
+            initialize();
+        }
+
+    }
+
+
+    /**
+     * Sets the
+     * 
+     * @param autoInitialize
+     */
+    public void setAutoInitialize(boolean autoInitialize) {
+
+        this.autoInitialize = autoInitialize;
     }
 
 }
